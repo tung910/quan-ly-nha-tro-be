738039e397fccf70b0c492fa44f20197d6fe6bea
VuongWeb <97082985+VuongWeb@users.noreply.github.com>
2022-09-11 13:41:24 +0700

    feat: model user

---
+++ src/models/user.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+import createHmac from 'crypto';
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 5
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+userSchema.methods = {
+    authenticate(password){
+        return this.password == this.encryPassword(password);
+    },
+    encryPassword(password){
+        if(!password) return;
+        try {
+            return createHmac('sha256','datn_tw13').update(password).digest('hex');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+userSchema.pre('save',function(next){
+    this.password = this.encryPassword(this.password);
+    next();
+})
+
+export default mongoose.model('User',userSchema);
