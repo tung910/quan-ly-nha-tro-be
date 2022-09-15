1dcae324124be6ff69dce173429922428f5ec81e
VuongWeb <97082985+VuongWeb@users.noreply.github.com>
2022-09-15 21:27:50 +0700

    feat: create router auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const motelRoomRoute = require('./motel-room.route');
+const userRouter = require('./user.router');
 
 function routes(app) {
     app.use('/api/motel-room', motelRoomRoute);
+    app.use('/api/users', userRouter)
 }
 
 module.exports = routes;
