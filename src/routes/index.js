e1e7645910005af966e8752d7563f6147754e003
long2512 <buidanglong02@gmail.com>
2022-09-15 22:27:44 +0700

    feat: router-motel

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const motelRoomRoute = require('./motel-room.route');
+const motelRoute = require('./motel.route')
 
 function routes(app) {
     app.use('/api/motel-room', motelRoomRoute);
+    app.use('/api/motel',motelRoute)
 }
 
 module.exports = routes;
