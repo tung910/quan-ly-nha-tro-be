8f48acb0b7aeb156950778ce85fce62e6474e2be
NamDD <donam17102002@gmail.com>
2022-09-16 21:17:36 +0700

    QLNTB-8 refactor: update edit customer

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -12,12 +12,16 @@ module.exports = {
         return AppResponse.success(req, res)(customer);
     }),
     editCustomer: asyncUtil(async (req, res) => {
-        const customer = await CustomerModel.findOneAndUpdate(
-            { _id: req.params.id },
-            req.body,
-            { new: true }
-        ).exec();
-        return AppResponse.success(req, res)(customer);
+        try {
+            const customer = await CustomerModel.findOneAndUpdate(
+                { _id: req.params.id },
+                req.body,
+                { new: true }
+            ).exec();
+            return AppResponse.success(req, res)(customer);
+        } catch (error) {
+            return AppResponse.fail(req,res)(error)
+        }
     }),
     deleteCustomer: asyncUtil(async (req, res) => {
         try {
