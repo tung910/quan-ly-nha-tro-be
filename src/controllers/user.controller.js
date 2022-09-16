128118e6c931658227dd27a5788ce0333cbbe193
VuongWeb <97082985+VuongWeb@users.noreply.github.com>
2022-09-17 02:51:03 +0700

    feat: create controller/auth

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,33 +2,9 @@ const UserModel = require("~/models/user.model");
 const asyncUtil = require('~/helpers/asyncUtil');
 const AppResponse = require('~/helpers/response');
 
-
 module.exports = {
-    signin: asyncUtil(async (req, res) => {
-        const { email, password } = req.body;
-        const user = await (await UserModel.findOne({ email })).save();
-        if (!user) {
-            return AppResponse.fail(res, req);
-        }
-        if (!user.authenticate(password)) {
-            return AppResponse.fail(res, req);
-        }
-        const token = jwt.sign({ _id: user._id }, "datn_tw13", { expiresIn: 60 * 60 });
-        return AppResponse.success(token, user)
+    getAllUsers: asyncUtil(async (req, res) => {
+        const users = await UserModel.findAll();
+        return AppResponse.success(req,res)(users);
     }),
-
-    signup: asyncUtil(asyncUtil(async (req, res) => {
-        const { email, name, password } = req.body;
-        try {
-            const existUser = await UserModel.findOne({ email }).exec();
-            if (existUser) {
-                return AppResponse.fail(req, res);
-            }
-            const user = await UserModel({ email, name, password }).save();
-            return AppResponse.success(req, res)(user);
-        } catch (error) {
-            return AppResponse.fail(req, res);
-        }
-    }))
-}
-
+};
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('~/controllers/user.controller');
+const authController = require('~/controllers/auth.controller');
 
-router.post('/signin', userController.signin);
-router.post('/signup', userController.signup);
+router.post('/signin', authController.signin);
+router.post('/signup', authController.signup);
+router.get('/users', userController.getAllUsers);
 
 module.exports = router;
