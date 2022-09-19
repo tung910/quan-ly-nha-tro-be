21bb8d865eff42c859dead1a46bbcbc6254d0bf6
VuongWeb <97082985+VuongWeb@users.noreply.github.com>
2022-09-19 22:23:03 +0700

    QLNTB-1 feat: crate api send email after register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,31 +2,59 @@ const UserModel = require("~/models/user.model");
 const asyncUtil = require('~/helpers/asyncUtil');
 const jwt = require('jsonwebtoken');
 const AppResponse = require('~/helpers/response');
-
+const nodemailer = require('nodemailer');
 
 module.exports = {
     signin: asyncUtil(async (req, res) => {
-        const { email, password } = req.body;
+        const { email, password, phoneNumber } = req.body;
         const user = await UserModel.findOne({ email }).exec();
         if (!user) {
             return AppResponse.fail(res, req);
         }
+        // if (!password) {
+        //     return AppResponse.fail(res, req);
+        // }
         if (!user.authenticate(password)) {
             return AppResponse.fail(res, req);
         }
         const token = jwt.sign({ _id: user._id }, "datn_tw13", { expiresIn: 60 * 60 });
         return AppResponse.success(req, res)(token, user)
-        
     }),
 
     signup: asyncUtil(async (req, res) => {
         const { email, name, password } = req.body;
-        const existUser = await UserModel.findOne({ email }).exec();
-        if (existUser) {
+        try {
+            const existUser = await UserModel.findOne({ email }).exec();
+            if (existUser) {
+                return AppResponse.fail(req, res);
+            }
+            let transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                    user: "vuonglvph15121@fpt.edu.vn",
+                    pass: "eivechqvbvxvpzkn",
+                },
+            });
+            await transporter.sendMail({
+                from: 'vuonglvph15121@fpt.edu.vn',
+                to: `${email}`,
+                subject: "TRỌ VƯƠNG ANH XIN CHÀO THÀNH VIÊN MỚI!",
+                html: `<p>Trọ Vương Anh xin cảm ơn bạn ${name} đã lựa chọn dịch vụ của chúng tôi! 
+                    Mọi thắc mắc xin liên hệ qua số điện thoại : <b>033333333</b> </p><br><b>Trân trọng!</b>`,
+            }, (error) => {
+                if (error) {
+                    return AppResponse.fail(error, res);
+                }
+                return res.json({
+                    message: `gửi mail thành công đến địa chỉ email: ${email}`
+                })
+            });
+            const user = await UserModel(req.body).save();
+            return AppResponse.success(req, res)(user);
+        } catch (error) {
             return AppResponse.fail(req, res);
         }
-        const user = await UserModel(req.body).save();
-        return AppResponse.success(req, res)(user);
+
     })
 }
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const createHmac = require('crypto');
+const { v4: uuidv4 } = require('uuid');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -28,29 +29,31 @@ const UserSchema = new mongoose.Schema({
         required: true
     }, role: {
         type: Number,
-        default:1
+        default: 1
     }
 
 }, { timestamps: true });
 
+
+UserSchema.pre('save', function (next) {
+    this.salt = uuidv4();
+    this.password = this.encryptPassword(this.password);
+    next();
+});
+
 UserSchema.methods = {
     authenticate(password) {
-        return this.password == this.encryPassword(password);
+        return this.password === this.encryptPassword(password);
     },
-    encryPassword(password) {
+    encryptPassword(password) {
         if (!password) return;
         try {
-            return createHmac('sha256', 'datn_tw13').update(password).digest('hex');
+            return createHmac('sha256', this.salt).update(password).digest('hex');
         } catch (error) {
             console.log(error);
         }
     }
 }
-
-UserSchema.pre('save', function (next) {
-    this.password = this.encryPassword(this.password);
-    next();
-})
 
 module.exports = mongoose.model('User', UserSchema);
 
