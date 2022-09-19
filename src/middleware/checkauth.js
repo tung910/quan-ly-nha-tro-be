c3c7f3e324b6443b067ed16edeb623cbb819aa9a
VuongWeb <97082985+VuongWeb@users.noreply.github.com>
2022-09-19 22:22:52 +0700

    QLNTB-1 feat: check auth user

---
+++ src/middleware/checkauth.js
@@ -0,0 +1,41 @@
+var { expressjwt } = require("express-jwt");
+
+module.exports = {
+    checkAuth: (req, res, next) => {
+        const isAdmin = true;
+        if (isAdmin) {
+            next();
+        } else {
+            res.redirect('/');
+        }
+    }
+}
+module.exports = {
+    requireSignin: expressJWT({
+        secret: "datn_tw13",
+        algorithms: ["HS256"],
+        requestProperty: "auth"
+    })
+}
+
+module.exports = {
+    isAuth: (req, res, next) => {
+        const user = req.profile._id == req.auth._id;
+        if (!user) {
+            return res.status(402).json({
+                message: "Bạn không được phép truy cập"
+            })
+        }
+        next();
+    }
+}
+module.exports = {
+    isAdmin: (req, res, next) => {
+        if (req.profile.role == 0) {
+            return res.status(401).json({
+                message: "Ban khong phai la admin"
+            })
+        }
+        next();
+    }
+} 
