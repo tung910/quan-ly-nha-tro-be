26199265c1528f497e7690642806a75ffb976b05
VuongWeb <97082985+VuongWeb@users.noreply.github.com>
2022-09-22 22:33:38 +0700

    QLNTB-1 fixbub: create API login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,23 +3,43 @@ const asyncUtil = require('~/helpers/asyncUtil');
 const jwt = require('jsonwebtoken');
 const AppResponse = require('~/helpers/response');
 const nodemailer = require('nodemailer');
+const passport = require("passport");
+const passportConfig = require('../config/passport')
 
 module.exports = {
     signin: asyncUtil(async (req, res) => {
-        const { email, password, phoneNumber } = req.body;
+        const { email, name, phoneNumber } = req.body;
         const user = await UserModel.findOne({ email }).exec();
         if (!user) {
-            return AppResponse.fail(res, req);
+            return AppResponse.fail(req, res);
         }
-        // if (!password) {
+
+        // if (!user.authenticate(password)) {
         //     return AppResponse.fail(res, req);
         // }
-        if (!user.authenticate(password)) {
-            return AppResponse.fail(res, req);
-        }
-        const token = jwt.sign({ _id: user._id }, "datn_tw13", { expiresIn: 60 * 60 });
-        return AppResponse.success(req, res)(token, user)
+        // if (!user.password) {
+        //     return AppResponse.fail(req, res);
+        // }
+        // const token = jwt.sign({ _id: user._id }, "datn_tw13", { expiresIn: 60 * 60 });
+        // return AppResponse.success(req, res)(token, user)
+
+        passport.authenticate('local', { session: false }),
+            (req, res) => {
+                if (req.authenticated()) {
+                    const { _id, name, phoneNumber } = req.user
+                    const token = signToken(_id);
+                    res.cookie("access_token", token, { httpOnly: true, sameSite: true });
+                    res.status(200).json({
+                        isAuthenticated: true,
+                        user: { _id, name ,phoneNumber}
+                    })
+                }
+            }
+
     }),
+
+
+
 
     signup: asyncUtil(async (req, res) => {
         const { email, name, password } = req.body;
