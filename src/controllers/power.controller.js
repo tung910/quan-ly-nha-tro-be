eb586a334de3078dc6e49018c81f592db59f4a46
NamDD <donam17102002@gmail.com>
2022-09-22 20:34:45 +0700

    QLNTB-41 feat: create api get list power

--- src/controllers/power.controller.js
+++ src/controllers/power.controller.js
@@ -7,5 +7,9 @@ module.exports = {
         const { data } = req.body;
         const power = await PowerModel.create(data);
         return AppResponse.success(req, res)(power);
+    }),
+    getListPower: asyncUtil(async (req, res) => {
+        const power = await PowerModel.find({});
+        return AppResponse.success(req, res)(power);
     })
 };
--- src/routes/power.route.js
+++ src/routes/power.route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const powerCOntroller = require('~/controllers/power.controller');
+const powerController = require('~/controllers/power.controller');
 
-router.post('/create', powerCOntroller.createPower);
+router.post('/create', powerController.createPower);
+router.get('/list', powerController.getListPower);
 
 module.exports = router;
