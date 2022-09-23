c7fcf3c92df9f7e0c69daa06426f601e4274b1c5
NamDD <donam17102002@gmail.com>
2022-09-23 21:09:55 +0700

    QLNTB-26 refactor: update api edit power & get list power

--- src/controllers/power.controller.js
+++ src/controllers/power.controller.js
@@ -5,16 +5,24 @@ const AppResponse = require('~/helpers/response');
 module.exports = {
     createPower: asyncUtil(async (req, res) => {
         const { data } = req.body;
-        const power = await PowerModel.create(data);
+        const power = await PowerModel(data).save();
         return AppResponse.success(req, res)(power);
     }),
     updatePower: asyncUtil(async (req, res) => {
         const { data } = req.body;
-        const power = await PowerModel.create(data);
+        const power = await PowerModel.findOneAndUpdate(
+            { _id: req.params.id },
+            data,
+            { new: true }
+        );
         return AppResponse.success(req, res)(power);
     }),
     getListPower: asyncUtil(async (req, res) => {
-        const power = await PowerModel.find({}).populate("customerId");
+        const power = await PowerModel.find({})
+            .populate({ path: 'customerId', select: 'name' })
+            .populate({ path: 'motelRoomId', select: 'roomName' })
+            .populate({ path: 'motelId', select: 'name' });
+
         return AppResponse.success(req, res)(power);
     }),
 };
