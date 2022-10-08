b6e686baa4f8d9dc52f0f7e57e9b20b3b5bbdfa2
namddph17471 <namddph17471@fpt.edu.vn>
2022-10-08 20:46:57 +0700

    QLNTB-46

--- src/models/calculation.models.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const CalculationSchema = new mongoose.Schema(
-    {
-        
-    },
-    { collection: 'Calculation', timestamps: true }
-);
-
-module.exports = mongoose.model('Calculation', CalculationSchema);
