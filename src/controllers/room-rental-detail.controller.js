4d36fc1bfd61422a7d613d395d795ccca2c90323
VuongLV <vuonglvph15121@gmail.com>
2022-10-10 16:32:16 +0700

    QLNTB-55 fix conflig roomRentalDetail controller

--- src/controllers/room-rental-detail.controller.js
+++ src/controllers/room-rental-detail.controller.js
@@ -67,11 +67,7 @@ module.exports = {
         const {
             data: { CustomerInfo, Member, Service, Contract },
         } = req.body;
-<<<<<<< Updated upstream
-        if (CustomerInfo == '' || null) {
-=======
         if (CustomerInfo == '' || CustomerInfo == null) {
->>>>>>> Stashed changes
             console.log('yêu cầu nhập đủ thông tin!');
             const msg = 'yêu cầu nhập đủ thông tin!';
             return AppResponse.fail(req, res)(msg);
