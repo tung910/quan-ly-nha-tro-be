dc036b63fd075e6b367e2a67a82fdeded47dace7
thanhtung <trantungg910@gmail.com>
2022-10-14 20:19:55 +0700

    QLNTB-59 fix filter by motel

--- src/helpers/asyncUtil.js
+++ src/helpers/asyncUtil.js
@@ -1,8 +1,15 @@
+const AppResponse = require('~/helpers/response');
+
 const asyncUtil = (fn) =>
     function asyncUtilWrap(...args) {
         const fnReturn = fn(...args);
         const next = args[args.length - 1];
-        return Promise.resolve(fnReturn).catch((error) => next(error));
+        const req = args[0];
+        const res = args[1];
+        return Promise.resolve(fnReturn).catch((error) => {
+            AppResponse.fail(req, res)(null, 'Some error');
+            return next(error);
+        });
     };
 
 module.exports = asyncUtil;
